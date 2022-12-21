d353b373ccbc3477b7f9db8451b4442b1f06611b
eren-ay <81091542+eren-ay@users.noreply.github.com>
2022-12-22 00:05:56 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 void merge(vector<int> array, int begin, int mid, int end);
+void mergeSort(int array[], int  begin, int  end);
+void printArray(int *A, int size);
+
+
 vector<int> randomNumbers(int numberCounter)
 {
     srand((unsigned)time(NULL));
@@ -16,98 +20,78 @@ vector<int> randomNumbers(int numberCounter)
     return numbers;
 }
 
-void merge(vector<int> array, int begin, int mid, int end)
-{
-    auto const subArrayOne = mid - begin + 1;
-    auto const subArrayTwo = end - mid;
-
-    // Create temp arrays
-    auto *leftArray = new int[subArrayOne],
-         *rightArray = new int[subArrayTwo];
-
-    // Copy data to temp arrays leftArray[] and rightArray[]
-    for (auto i = 0; i < subArrayOne; i++)
-        leftArray[i] = array[begin + i];
-    for (auto j = 0; j < subArrayTwo; j++)
-        rightArray[j] = array[mid + 1 + j];
-
-    auto indexOfSubArrayOne = 0,    // Initial index of first sub-array
-        indexOfSubArrayTwo = 0;     // Initial index of second sub-array
-    int indexOfMergedArray = begin; // Initial index of merged array
-
-    // Merge the temp arrays back into array[left..right]
-    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+void merge(int* array, int mid, int end){
+    int tempBegin=0;
+    mid++;
+    int tempMid=mid;
+    
+    int *mergedArray=new int[end+1];
+    end++;
+    int mergedArrayIndex=0;
+    while (tempBegin < mid || tempMid < end)
     {
-        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        if (tempBegin < mid && tempMid < end)
         {
-            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-            indexOfSubArrayOne++;
-        }
-        else
+            if (array[tempBegin]<=array[tempMid]){
+                mergedArray[mergedArrayIndex]=array[tempBegin];
+                tempBegin++;
+            }
+            else{
+                mergedArray[mergedArrayIndex]=array[tempMid];
+                tempMid++;
+            }
+            
+        }else if (tempBegin < mid)
         {
-            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-            indexOfSubArrayTwo++;
+            mergedArray[mergedArrayIndex]=array[tempBegin++];    
+        }else if (tempMid < end)
+        {
+            mergedArray[mergedArrayIndex]=array[tempMid++];
         }
-        indexOfMergedArray++;
+        mergedArrayIndex++;
     }
-    // Copy the remaining elements of
-    // left[], if there are any
-    while (indexOfSubArrayOne < subArrayOne)
+    for (size_t i = 0; i < end; i++)
     {
-        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-        indexOfSubArrayOne++;
-        indexOfMergedArray++;
+        array[i]=mergedArray[i];
     }
-    // Copy the remaining elements of
-    // right[], if there are any
-    while (indexOfSubArrayTwo < subArrayTwo)
-    {
-        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-        indexOfSubArrayTwo++;
-        indexOfMergedArray++;
-    }
-    delete[] leftArray;
-    delete[] rightArray;
 }
 
-void mergeSort(vector<int> array, int begin, int end)
+
+void mergeSort(int array[], int  begin, int  end)
 {
+    
     if (begin >= end)
-    {
         return;
-    }
-    auto mid = (begin + (end - begin)) / 2;
+    
+    
+    auto mid = begin + (end - begin) / 2;
+
+    /* 2x2 thread< */
     mergeSort(array, begin, mid);
     mergeSort(array, mid + 1, end);
-    merge(array, begin, mid, end);
+    
+    mid = mid - begin;
+    end= end-begin;
+    merge(&array[begin],mid,end);
+    
 }
 
 void printArray(int *A, int size)
 {
-    A[1] = 5;
+    
     for (auto i = 0; i < size; i++)
         cout << A[i] << " ";
 }
 
 int main()
 {
-    vector<int> arr = randomNumbers(5);
-
+    vector<int> arr = randomNumbers(8);
     int arr_size = arr.size();
 
-    printArray(&arr[0], arr_size);
     cout << "Given array is \n";
-
-    mergeSort(arr, 0, arr_size - 1);
-
-    /*
-printArray(*arr, arr_size);
-
-mergeSort(*arr, 0, arr_size - 1);
-
-cout << "\nSorted array is \n";
-printArray(*arr, arr_size);
-*/
-
+    printArray(&arr[0], arr_size);
+    mergeSort(&arr[0], 0, arr_size-1 );
+    cout << "Sorted array is \n";
+    printArray(&arr[0], arr_size);
     return 0;
 }
